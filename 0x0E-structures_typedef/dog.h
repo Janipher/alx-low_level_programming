1e2b716f602b1c27bb69d4c5f6af3675280d44b0
Janipher <janipherowiny@gmail.com>
2023-01-18 01:41:15 -0800

    Updated header file.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,7 +14,7 @@ struct dog
 	float age;
 	char *owner;
 };
-typdef struct dog dog_t;
+#define typdef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
