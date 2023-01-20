3bc7dc77275c6af2b075067b716ec29d3192338f
Janipher <janipherowiny@gmail.com>
2023-01-20 07:59:41 -0800

    Updated header file and a file containing a function that returns the sum of all its parameters.

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - function that returns the sum of all its parameters
+ * @n: parameter
+ * Return: sum if successful, if not 0
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list jenny;
+	unsigned int j = 0;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(jenny, n);
+
+	for (j = 0; j < n; j++)
+		sum += va_arg(jenny, int);
+
+	va_end(jenny);
+	return (sum);
+}
